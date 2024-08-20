0c476d326b78d04ac7b4c182d385a6f896ec9fcd
Julio Mattos <julio.mattos999@gmail.com>
2024-08-19 21:05:30 -0300

    Create MultiploDe3.java

---
+++ MultiploDe3.java
@@ -0,0 +1,18 @@
+package AulaAgrVai;
+
+public class Principal {
+	
+    public static void main(String[] args) {
+        int soma = 0; 
+
+
+        for (int num = 1; num < 100; num++) {
+            if(num%3==0) {
+            	System.out.println("Multiplo de 3: " + num);
+            }
+            	
+        }       
+    
+}
+
+}
